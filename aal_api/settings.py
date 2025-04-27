301155cb8613b06f84a9bf6cf3566aaacf90c709
gilbertoloboac <loboacre@gmail.com>
2025-04-27 17:00:45 -0500

    foi criada a configuração para o deploy

--- aal_api/settings.py
+++ aal_api/settings.py
@@ -1,25 +1,22 @@
 import os
 from pathlib import Path
-
+from decouple import config  # Importando para usar variáveis de ambiente
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t(bi!_8^c&c#+%u3203k&e*-ys=_sy2zcd^yj#a-0)s@*!c#a%'
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-t(bi!_8^c&c#+%u3203k&e*-ys=_sy2zcd^yj#a-0)s@*!c#a%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['65.21.156.144', 'localhost']
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,7 +40,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
 ]
-
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
@@ -72,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'aal_api.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -82,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -102,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -114,12 +107,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
